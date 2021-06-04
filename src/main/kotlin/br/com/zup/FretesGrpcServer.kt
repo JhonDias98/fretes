0ef132461425fda4fdfec2132afd2e1ae4eb9dfa
Jonathan Pereira Dias <52224050+JhonDias98@users.noreply.github.com>
2021-06-04 01:19:28 -0300

    Retornando códigos de erro em uma API gRPC

--- src/main/kotlin/br/com/zup/FretesGrpcServer.kt
+++ src/main/kotlin/br/com/zup/FretesGrpcServer.kt
@@ -1,5 +1,9 @@
 package br.com.zup
 
+import com.google.rpc.Code
+import com.google.protobuf.Any
+import io.grpc.Status
+import io.grpc.protobuf.StatusProto
 import io.grpc.stub.StreamObserver
 import org.slf4j.LoggerFactory
 import javax.inject.Singleton
@@ -10,16 +14,60 @@ class FretesGrpcServer : FretesServiceGrpc.FretesServiceImplBase(){
 
     private val logger = LoggerFactory.getLogger(FretesGrpcServer::class.java)
 
-    override fun calculaFrete(request: CalculaFreteRequest?, responseObserver: StreamObserver<CalculaFreteResponse>?) {
+    override fun calculaFrete(
+        request: CalculaFreteRequest?,
+        responseObserver: StreamObserver<CalculaFreteResponse>?
+    ) {
         logger.info("Calculando frete para request: $request")
 
-        val response = CalculaFreteResponse.newBuilder()
-            .setCep(request!!.cep)
-            .setValor(Random.nextDouble(from = 0.0, until = 140.0))
-            .build()
-        logger.info("Frete calculado para o cep ${response.cep}, no valor de R$ ${response.valor}")
+        val cep = request!!.cep
 
-        responseObserver!!.onNext(response)
-        responseObserver.onCompleted()
+        if(cep == null || cep.isBlank()){
+            responseObserver?.onError(Status.INVALID_ARGUMENT
+                .withDescription("cep deve ser informado")
+                .asRuntimeException())
+            return
+        }
+
+
+        if(!request.cep.matches("[0-9]{5}-[\\d]{3}".toRegex())){
+            responseObserver?.onError(Status.INVALID_ARGUMENT
+                .withDescription("cep com formato inválido")
+                .augmentDescription("O formato aceito é 99999-999")
+                .asRuntimeException())
+            return
+        }
+
+
+        val valor: Double
+        try {
+            valor = Random.nextDouble(from = 0.0, until = 140.0)
+            if(valor > 100.0) {
+                throw IllegalStateException("Erro inesperado ao executar logica de negócio!")
+            }
+        } catch (e: Exception){
+            responseObserver?.onError(Status.INTERNAL
+                .withDescription(e.message)
+                .withCause(e) // anexado ao Status, mas não enviado ao Client
+                .asRuntimeException())
+        }
+
+        //Simula um verificador de segurança
+        if(cep.endsWith("333")){
+            val statusProto = com.google.rpc.Status.newBuilder()
+                .setCode(Code.PERMISSION_DENIED.number)
+                .setMessage("usuário sem acesso ao recurso selecionado")
+                .addDetails(Any.pack(
+                    ErrorDetails.newBuilder()
+                        .setCode(401)
+                        .setMessage("Token expirado")
+                        .build())
+                )
+                .build()
+
+            responseObserver?.onError(
+                StatusProto.toStatusRuntimeException(statusProto)
+            )
+        }
     }
 }
